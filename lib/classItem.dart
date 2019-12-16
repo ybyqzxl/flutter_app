0b2c2f512cde54ee24e59e772a714364a7aec022
zhaoxueli <zhaoxueli@100tal.com>
2019-12-16 20:29:05 +0800

    1、增加资源

--- lib/classItem.dart
+++ lib/classItem.dart
@@ -3,34 +3,45 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: ClassCardItemWidget(
-        model: ClassItemModel(
-            className: "assets/icon.png",
-            classTime:
-                "Thanks for using Google Maps! This release brings bug fixes "
-                "that improve our product to help you discover new places and navigate to them.",
-            classAddress: "Google Maps - Transit & Fond",
-            classDate: "137.2",
-            classType: "2019年6月5日"),
-        onPressed: () {},
-      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("classItemTest"),
+          ),
+          body: ListView(
+            children: <Widget>[
+              ClassCardItemWidget(
+                model: ClassItemModel(
+                    className: "双师测试课程",
+                    classTime: "周六、周日",
+                    classAddress: "海淀黄庄",
+                    classDate: "2019-07-08",
+                    classMajorTeacher: "周杰伦",
+                    classType: 1,
+                    classTeacher: [
+                      "张三",
+                      "李四",
+                      "王五",
+                      "张三",
+                      "李四",
+                      "王五",
+                      "张三",
+                      "李四",
+                      "王五"
+                    ],
+                    classPrice: "2000",
+//                    followPeople: "1",
+                    classState: "已满"),
+                onPressed: () {},
+              ),
+            ],
+          )),
     );
   }
 }
@@ -40,18 +51,24 @@ class ClassItemModel {
   String classTime;
   String classAddress;
   String classDate;
-  String classType;
+  int classType;
+  String classMajorTeacher;
   List<String> classTeacher;
   String classPrice;
+  String followPeople;
+  String classState;
 
   ClassItemModel(
       {this.className,
       this.classTime,
       this.classAddress,
       this.classDate,
+      this.classMajorTeacher,
       this.classType,
       this.classTeacher,
-      this.classPrice});
+      this.classPrice,
+      this.followPeople,
+      this.classState});
 }
 
 class ClassCardItemWidget extends StatelessWidget {
@@ -60,16 +77,40 @@ class ClassCardItemWidget extends StatelessWidget {
   ClassCardItemWidget({Key key, this.model, this.onPressed}) : super(key: key);
   final VoidCallback onPressed;
 
+  String _getClassType() {
+    switch (model.classType) {
+      case 1:
+        return "assets/online_label.png";
+      case 2:
+        return "assets/double_lable.png";
+      case 4:
+        return "assets/face_lable.png";
+    }
+    return "";
+  }
+
   @override
   Widget build(BuildContext context) {
     //组合上下两部分
-    return Container(
-      child: Column(
-          children: <Widget>[buildTopRow(context), buildBottomRow(context)]),
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.fromLTRB(15, 17, 0, 0),
-      height: 190,
-      width: double.maxFinite,
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
+          child: Column(children: <Widget>[
+            buildTopRow(context),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 12, 0, 0),
+              height: 1,
+              color: Color(0xffe5e5e5),
+            ),
+            buildBottomRow(context),
+          ]),
+        ),
+        Container(
+          height: 10,
+          color: Color(0xfff4f5f6),
+        ),
+      ],
     );
   }
 
@@ -77,53 +118,89 @@ class ClassCardItemWidget extends StatelessWidget {
   Widget buildTopRow(BuildContext context) {
     return Column(
       children: <Widget>[
-        Row(children: <Widget>[
-          Expanded(
-            child: Text(
-              model.className,
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-                color: Color(0x1C234B),
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: Row(
+            children: <Widget>[
+              Image.asset(
+                _getClassType(),
+                width: 28,
+                height: 15,
+                fit: BoxFit.fill,
               ),
-            ),
-          ),
-          Image.asset("assets/icon.png"),
-        ]),
-        Row(
-          children: <Widget>[
-            Image.asset("assets/icon.png"),
-            Expanded(
-              child: Text(
-                model.classTime,
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Color(0x1C234B),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                  child: Text(
+                    model.className,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xff000000),
+                    ),
+                  ),
                 ),
               ),
-            )
-          ],
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
+          child: Row(
+            children: <Widget>[
+              Image.asset(
+                "assets/icon_time.png",
+                width: 12,
+                height: 14,
+                fit: BoxFit.fill,
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                  child: Text(
+                    model.classTime,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xff626b80),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
         Row(
           children: <Widget>[
-            Image.asset("assets/icon.png"),
+            Image.asset(
+              "assets/icon_address.png",
+              width: 12,
+              height: 14,
+              fit: BoxFit.fill,
+            ),
             Expanded(
-              child: Text(
-                model.classAddress,
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Color(0x1C234B),
+              child: Container(
+                margin: EdgeInsets.fromLTRB(6, 0, 0, 0),
+                child: Text(
+                  model.classAddress,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xff626b80),
+                  ),
                 ),
               ),
             )
           ],
         ),
         Container(
-          margin: EdgeInsets.fromLTRB(0, 24, 0, 0),
-          child: Text(model.classDate),
+          alignment: Alignment.topLeft,
+          margin: EdgeInsets.fromLTRB(0, 12, 0, 0),
+          child: Text(
+            "授课：${model.classMajorTeacher}",
+            textAlign: TextAlign.left,
+            style: TextStyle(fontSize: 12, color: Color(0xff666666)),
+          ),
         ),
       ],
     );
@@ -131,53 +208,129 @@ class ClassCardItemWidget extends StatelessWidget {
 
   //创建下半部分
   Widget buildBottomRow(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-        child: Row(children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemCount: model.classTeacher.length,
-              itemExtent: 35,
-              itemBuilder: itemView,
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 8, 0, 0),
+      child: Row(children: <Widget>[
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 0, 8, 0),
+          height: 60,
+          width: 166,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            itemCount: model.classTeacher.length,
+            itemBuilder: itemView,
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Text.rich(
+                  TextSpan(children: [
+                    TextSpan(
+                      text: "\$",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Colors.red),
+                    ),
+                    TextSpan(
+                      text: "${model.classPrice}",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.red),
+                    ),
+                  ]),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Offstage(
+                      offstage: _followedPeople(),
+                      child: Text("${model.followPeople}人以关注"),
+                    ),
+                    _classState(),
+                  ],
+                )
+              ],
             ),
           ),
-          Text.rich(
-            TextSpan(children: [
-              TextSpan(
-                text: "\$",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12,
-                    color: Colors.red),
-              ),
-              TextSpan(
-                text: "${model.classPrice}",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.red),
-              ),
-            ]),
+        ),
+      ]),
+    );
+  }
+
+  bool _followedPeople() {
+    if (model.followPeople == null) {
+      return true;
+    }
+    return false;
+  }
+
+  Widget _classState() {
+    if (model.classState != null) {
+      if (model.classState == "已满") {
+        return Container(
+          decoration: BoxDecoration(
+            color: Color(0xffF5A623),
+            borderRadius: BorderRadius.circular(4),
           ),
-        ]));
+          child: Text(
+            model.classState,
+            style: TextStyle(
+              color: Color(0xfff5a623),
+              fontSize: 12,
+            ),
+          ),
+        );
+      } else {
+        return Container(
+          decoration: BoxDecoration(
+            color: Color(0xff62D45D),
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Text(
+            "剩余${model.classState}",
+            style: TextStyle(
+              color: Color(0xff62D45D),
+              fontSize: 12,
+            ),
+          ),
+        );
+      }
+    }
   }
 
   /// ListView 中每一行的视图
   Widget itemView(BuildContext context, int index) {
-    return Row(
-      children: <Widget>[
-        Image.asset("assets/icon.png"),
-        Container(
-          margin: EdgeInsets.fromLTRB(6, 0, 0, 0),
-          child: Text(
-            model.classTeacher[index],
-            style: TextStyle(
-              fontSize: 14,
-              color: Color(0x1C234B),
-            ),
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.fromLTRB(0, 0, 12, 0),
+      child: Column(
+        children: <Widget>[
+          Image.asset(
+            "assets/icon_mine.png",
+            width: 32,
+            height: 32,
+            fit: BoxFit.fill,
           ),
-        )
-      ],
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+            child: Text(
+              model.classTeacher[index],
+              style: TextStyle(
+                fontSize: 11,
+                color: Color(0xff666666),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }

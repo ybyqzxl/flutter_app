151272222ad1248808d51bfce5fdcab8f60adee6
zhaoxueli <zhaoxueli@100tal.com>
2019-12-18 15:43:24 +0800

    1、完善

--- lib/classItem.dart
+++ lib/classItem.dart
@@ -10,43 +10,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("classItemTest"),
-          ),
-          body: ListView(
-            children: <Widget>[
-              ClassCardItemWidget(
-                model: ClassItemModel(
-                    className: "双师测试课程",
-                    classTime: "周六、周日",
-                    classAddress: "海淀黄庄",
-                    classDate: "2019-07-08",
-                    classMajorTeacher: "周杰伦",
-                    classType: 1,
-                    classTeacher: [
-                      "张三",
-                      "李四",
-                      "王五",
-                      "张三",
-                      "李四",
-                      "王五",
-                      "张三",
-                      "李四",
-                      "王五"
-                    ],
-                    classPrice: "2000",
-//                    followPeople: "1",
-                    classState: "已满"),
-                onPressed: () {},
-              ),
-            ],
-          )),
+      home: MyFocusScreen(),
     );
   }
 }
 
+class MyFocusScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyFocusState();
+  }
+}
+
 class ClassItemModel {
+  String classId;
   String className;
   String classTime;
   String classAddress;
@@ -57,8 +34,9 @@ class ClassItemModel {
   String classPrice;
   String followPeople;
   String classState;
+  bool classChecked;
 
-  ClassItemModel(
+  ClassItemModel(this.classId,
       {this.className,
       this.classTime,
       this.classAddress,
@@ -68,14 +46,284 @@ class ClassItemModel {
       this.classTeacher,
       this.classPrice,
       this.followPeople,
-      this.classState});
+      this.classState,
+      this.classChecked});
+}
+
+class MyFocusState extends State<MyFocusScreen> {
+  String _menu = "编辑";
+  bool _isShowCheckedAll = true;
+
+  void changeCheckedAll(bool changeChecked) {
+    setState(() {
+//      _isShowCheckedAll = changeChecked;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => {},
+            padding: EdgeInsets.all(18),
+            icon: Image.asset("assets/icon_back.png"),
+          ),
+          centerTitle: true,
+          elevation: 0,
+          actions: <Widget>[
+            GestureDetector(
+              onTap: () => setState(() {
+                if (_menu == "编辑") {
+                  _isShowCheckedAll = false;
+                  _menu = "完成";
+                } else {
+                  _isShowCheckedAll = true;
+                  _menu = "编辑";
+                }
+                for (var value in classItems) {
+                  value.classChecked = false;
+                }
+              }),
+              child: Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                child: Text(
+                  _menu,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            )
+          ],
+          backgroundColor: Colors.white,
+          title: Text(
+            "关注的班级",
+            style: TextStyle(color: Color(0xff132c4b), fontSize: 18),
+          ),
+        ),
+        body: ContentWidget(_isShowCheckedAll, changeCheckedAll));
+  }
+}
+
+final List<ClassItemModel> classItems = [
+  ClassItemModel("1",
+      className: "双师测试课程",
+      classTime: "周六、周日",
+      classAddress: "海淀黄庄",
+      classDate: "2019-07-08",
+      classMajorTeacher: "周杰伦",
+      classType: 1,
+      classTeacher: ["张三", "李四", "王五", "张三", "李四", "王五", "张三", "李四", "王五"],
+      classPrice: "2000",
+      followPeople: "1",
+      classState: "增开",
+      classChecked: false),
+  ClassItemModel("2",
+      className: "双师测试课程",
+      classTime: "周六、周日",
+      classAddress: "海淀黄庄",
+      classDate: "2019-07-08",
+      classMajorTeacher: "周杰伦",
+      classType: 1,
+      classTeacher: ["张三", "李四", "王五", "张三", "李四", "王五", "张三", "李四", "王五"],
+      classPrice: "2000",
+      followPeople: "1",
+      classState: "增开",
+      classChecked: false),
+  ClassItemModel("3",
+      className: "双师测试课程",
+      classTime: "周六、周日",
+      classAddress: "海淀黄庄",
+      classDate: "2019-07-08",
+      classMajorTeacher: "周杰伦",
+      classType: 1,
+      classTeacher: ["张三", "李四", "王五", "张三", "李四", "王五", "张三", "李四", "王五"],
+      classPrice: "2000",
+      followPeople: "1",
+      classState: "增开",
+      classChecked: false),
+  ClassItemModel("4",
+      className: "双师测试课程",
+      classTime: "周六、周日",
+      classAddress: "海淀黄庄",
+      classDate: "2019-07-08",
+      classMajorTeacher: "周杰伦",
+      classType: 1,
+      classTeacher: ["张三", "李四", "王五", "张三", "李四", "王五", "张三", "李四", "王五"],
+      classPrice: "2000",
+      followPeople: "1",
+      classState: "增开",
+      classChecked: false)
+];
+
+class ContentWidget extends StatefulWidget {
+  final bool _isShowCheckedAll;
+
+  final ValueChanged<bool> checkedAllChange;
+
+  ContentWidget(this._isShowCheckedAll, this.checkedAllChange);
+
+  @override
+  State<StatefulWidget> createState() {
+    return ContentWidgetState();
+  }
+}
+
+class ContentWidgetState extends State<ContentWidget> {
+  bool _allChecked = false;
+
+  bool _isHaveClassChecked = false;
+  final List<ClassItemModel> mList = List();
+
+  void _isAllChecked() {
+    if (classItems.isEmpty) {
+      return;
+    }
+    _allChecked = true;
+    for (var value in classItems) {
+      if (!value.classChecked) {
+        _allChecked = false;
+        break;
+      }
+    }
+    if (widget._isShowCheckedAll) {
+      _isHaveClassChecked = false;
+      mList.clear();
+    }
+  }
+
+  void _onChecked(ClassItemModel model) {
+    if (model.classChecked) {
+      if (!mList.contains(model)) {
+        mList.add(model);
+      }
+    } else {
+      mList.remove(model);
+    }
+    setState(() {
+      _isHaveClassChecked = mList.isNotEmpty;
+      print(_isHaveClassChecked);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _isAllChecked();
+    return Container(
+      color: Color(0xFFF4F5F6),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Container(
+            height: 1,
+            color: Color(0xffe5e5e5),
+          ),
+          Offstage(
+            offstage: widget._isShowCheckedAll,
+            child: Container(
+              width: double.infinity,
+              color: Colors.white,
+              padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      for (var value in classItems) {
+                        value.classChecked = !_allChecked;
+                      }
+                      if (!_allChecked) {
+                        mList.clear();
+                        mList.addAll(classItems);
+                      } else {
+                        mList.clear();
+                      }
+                      setState(() {
+                        _isHaveClassChecked = mList.isNotEmpty;
+                        _isAllChecked();
+                      });
+                    },
+                    child: Image.asset(
+                      _allChecked
+                          ? "assets/icon_checked.webp"
+                          : "assets/icon_uncheck.webp",
+                      width: 18,
+                      height: 18,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                      child: Text(
+                        "全选",
+                        style:
+                            TextStyle(color: Color(0xff333333), fontSize: 15),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: classItems == null ? 0 : classItems.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ClassCardItemWidget(
+                  model: classItems[index],
+                  isShowCheckedAll: widget._isShowCheckedAll,
+                  onChecked: _onChecked,
+                );
+              },
+            ),
+          ),
+          Offstage(
+            offstage: widget._isShowCheckedAll,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
+                onTap: () {
+                  for (var value in mList) {
+                    classItems.remove(value);
+                  }
+                  _isHaveClassChecked = false;
+                  mList.clear();
+                  widget.checkedAllChange(true);
+                },
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.bottomCenter,
+                  color:
+                      _isHaveClassChecked ? (Colors.blue) : Color(0xffcccccc),
+                  padding: EdgeInsets.fromLTRB(0, 11, 0, 11),
+                  child: Text(
+                    "取消关注",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class ClassCardItemWidget extends StatelessWidget {
   final ClassItemModel model;
 
-  ClassCardItemWidget({Key key, this.model, this.onPressed}) : super(key: key);
-  final VoidCallback onPressed;
+  final bool isShowCheckedAll;
+  final ValueChanged<ClassItemModel> onChecked;
+
+  ClassCardItemWidget(
+      {Key key, this.model, @required this.isShowCheckedAll, this.onChecked})
+      : super(key: key);
 
   String _getClassType() {
     switch (model.classType) {
@@ -92,26 +340,67 @@ class ClassCardItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //组合上下两部分
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
-          child: Column(children: <Widget>[
-            buildTopRow(context),
-            Container(
-              margin: EdgeInsets.fromLTRB(0, 12, 0, 0),
-              height: 1,
-              color: Color(0xffe5e5e5),
-            ),
-            buildBottomRow(context),
-          ]),
-        ),
-        Container(
-          height: 10,
-          color: Color(0xfff4f5f6),
-        ),
-      ],
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              _buildSelect(context),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
+                      child: Column(children: <Widget>[
+                        buildTopRow(context),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 12, 0, 0),
+                          height: 1,
+                          color: Color(0xffe5e5e5),
+                        ),
+                        buildBottomRow(context),
+                      ]),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 10,
+            color: Color(0xfff4f5f6),
+          ),
+        ],
+      ),
     );
+  }
+
+  Widget _buildSelect(BuildContext context) {
+    return Offstage(
+      offstage: isShowCheckedAll,
+      child: GestureDetector(
+        onTap: () {
+          model.classChecked = !model.classChecked;
+          onChecked(model);
+        },
+        child: Container(
+          margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
+          child: Image.asset(
+            _getChecked(),
+            width: 18,
+            height: 18,
+          ),
+        ),
+      ),
+    );
+  }
+
+  String _getChecked() {
+    if (model.classChecked) {
+      return "assets/icon_checked.webp";
+    }
+    return "assets/icon_uncheck.webp";
   }
 
   //创建上半部分
@@ -211,54 +500,63 @@ class ClassCardItemWidget extends StatelessWidget {
     return Container(
       margin: EdgeInsets.fromLTRB(0, 8, 0, 0),
       child: Row(children: <Widget>[
-        Container(
-          margin: EdgeInsets.fromLTRB(0, 0, 8, 0),
-          height: 60,
-          width: 166,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            shrinkWrap: true,
-            itemCount: model.classTeacher.length,
-            itemBuilder: itemView,
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(0, 4, 8, 0),
+            height: 60,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemCount: model.classTeacher.length,
+              itemBuilder: itemView,
+            ),
           ),
         ),
         Expanded(
-          flex: 2,
-          child: Container(
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                Text.rich(
-                  TextSpan(children: [
-                    TextSpan(
-                      text: "\$",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                          color: Colors.red),
-                    ),
-                    TextSpan(
-                      text: "${model.classPrice}",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.red),
-                    ),
-                  ]),
-                ),
-                Row(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Text.rich(
+                TextSpan(children: [
+                  TextSpan(
+                    text: "\$",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                        color: Colors.red),
+                  ),
+                  TextSpan(
+                    text: "${model.classPrice}",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.red),
+                  ),
+                ]),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
                     Offstage(
                       offstage: _followedPeople(),
-                      child: Text("${model.followPeople}人以关注"),
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 4, 0),
+                        child: Text(
+                          "${model.followPeople}人以关注",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Color(0xff999999),
+                          ),
+                        ),
+                      ),
                     ),
                     _classState(),
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+            ],
           ),
         ),
       ]),
@@ -276,8 +574,9 @@ class ClassCardItemWidget extends StatelessWidget {
     if (model.classState != null) {
       if (model.classState == "已满") {
         return Container(
+          padding: EdgeInsets.fromLTRB(8, 1, 8, 1),
           decoration: BoxDecoration(
-            color: Color(0xffF5A623),
+            border: Border.all(color: Color(0xffF5A623), width: 1),
             borderRadius: BorderRadius.circular(4),
           ),
           child: Text(
@@ -290,8 +589,9 @@ class ClassCardItemWidget extends StatelessWidget {
         );
       } else {
         return Container(
+          padding: EdgeInsets.fromLTRB(8, 1, 8, 1),
           decoration: BoxDecoration(
-            color: Color(0xff62D45D),
+            border: Border.all(color: Color(0xff62D45D), width: 1),
             borderRadius: BorderRadius.circular(4),
           ),
           child: Text(
@@ -304,6 +604,13 @@ class ClassCardItemWidget extends StatelessWidget {
         );
       }
     }
+    return Offstage(
+      offstage: true,
+      child: Container(
+        width: 0,
+        height: 0,
+      ),
+    );
   }
 
   /// ListView 中每一行的视图
